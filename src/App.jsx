20371dd104b0deb7a2d4075ad260fbf27f9799c3
Elvin Moises Sanchez Medina <elvin@clinpays.com>
2022-05-14 09:17:59 -0600

    structure initial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='flex flex-row justify-center'>
+			<p className='text-3xl font-bold underline'>Guanatos APP</p>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+const ExamplePage = () => <h2>Page de ejemplo</h2>;
+
+export default ExamplePage;
